2fabde6649c6c89d1a0a8f17efba49ce31e5137a
Maksym Nykyforuk <max.nik3032@gmail.com>
2020-04-03 10:29:18 +0300

    added classes for group

---
+++ src/main/java/ua/lviv/iot/spring/rest/business/GroupService.java
@@ -0,0 +1,18 @@
+package ua.lviv.iot.spring.rest.business;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ua.lviv.iot.spring.rest.dataaccess.GroupRepository;
+import ua.lviv.iot.spring.rest.model.Group;
+
+import java.util.List;
+
+@Service
+public class GroupService {
+    @Autowired
+    private GroupRepository groupRepository;
+
+    public List<Group> findAll() {
+        return groupRepository.findAll();
+    }
+}
